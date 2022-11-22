6ca8cbf17fae92e7c874cac7165817708bfc0469
Long Nguyen <longewinn@gmail.com>
2022-11-21 19:44:34 -0600

    Pushing any changes

--- budget.py
+++ budget.py
@@ -1,1 +1,2 @@
 import budget_app
+
--- budget_app.py
+++ budget_app.py
@@ -1,13 +1,13 @@
 import budget
 
-import tkinter as tk
+from tkinter import *
 
-root = tk.Tk()
-root.title('Budget Planner for Broke Bitches')
-open_button = tk.Button(root, text='Add budget')
-open_button.pack(side='top')
-canvas = tk.Canvas(root, width=800, height=400, bg='white')
+root = Tk()
+
+root.title('Budget for Broke Bitches')
+budget_button = Button(root, text='Add Budget')
+budget_button.pack(side='top')
+canvas = Canvas(root, width=800, height=600, bg='white')
 canvas.pack(side='bottom')
-canvas.create_text(text='Ello mate')
 
-tk.mainloop()
+root.mainloop()
