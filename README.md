9280a5dcbb40f2ed97c3b853b887afd6a60e0ecb
Long Nguyen <longewinn@gmail.com>
2023-03-06 11:14:15 -0600

    Adding elements to the front-end

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-To start Kivy Environment: source /Users/longnguyen/Projects/budget-app/kivy_venv/bin/activate
+1) start Kivy Environment: source /Users/longnguyen/Projects/budget-app/kivy_venv/bin/activate
+
+2) run app with python budgetApp.py
+
+3) ctrl + C to stop app
--- budgetApp.py
+++ budgetApp.py
@@ -5,14 +5,25 @@ kv = '''
 ScreenManager:
     Screen:
         name: 'main_screen'
-    BoxLayout:
-        orientation: 'vertical'
-        Label: 
-            text: 'Budget for Broke Bitches'
-        Button:
-            text: 'Budget Button'
-            on_press: budgetManager.addBudget()
-        
+        BoxLayout:
+            orientation: 'vertical'
+            Label:
+                text: 'Budget for a Brokey'
+            BoxLayout:
+                orientation: 'vertical'
+                BoxLayout:
+                    orientation: 'horizontal'
+                    Label:
+                        text: 'Input the amount of money you have for the month:'
+                    TextInput:
+                        text: 'Amount'
+                BoxLayout:
+                    orientation: 'horizontal'
+                    Label: 
+                        text: 'Input the categories you want to spend your money on for this month, separated by commas:'
+                    TextInput: 
+                        text: 'Categories'
+
 '''
 
 class BudgetApp(App):
